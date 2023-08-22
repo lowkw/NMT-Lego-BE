a9488868a63a9299dce8cdb6e37b094bdfb8eeed
Cameron <20095325@tafe.wa.edu.au>
2023-08-22 11:28:50 +0800

    Update legoSet.php

--- app/Models/legoSet.php
+++ app/Models/legoSet.php
@@ -12,13 +12,10 @@ class legoSet extends Model
     protected $fillable = [
         'set_number',
         'name',
-        'description',
         'year',
         'theme_id',
         'num_parts',
-        'set_img_url',
-        'size',
-        'type',
+        'set_img_url'
     ];
     /**
      * The attributes that should be cast.
