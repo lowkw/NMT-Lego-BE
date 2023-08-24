adf070d40fc794c085392d789afb3a10b39339f2
Cameron <20095325@tafe.wa.edu.au>
2023-08-24 12:12:30 +0800

    Generated themes controller, model, migration, seeder

---
+++ app/Http/Controllers/ThemesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorethemesRequest;
+use App\Http\Requests\UpdatethemesRequest;
+use App\Models\themes;
+
+class ThemesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorethemesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(themes $themes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(themes $themes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatethemesRequest $request, themes $themes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(themes $themes)
+    {
+        //
+    }
+}
