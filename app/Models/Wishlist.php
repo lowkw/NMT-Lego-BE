722a7b523befcd9a08dcb4b9a3a3fd05b73d8546
NicholasL-Tafe <20101509@tafe.wa.edu.au>
2023-09-07 16:30:45 +0800

    SCRUM 30,31 -
    Created Wishlist Model,Controller,Migration.
    
    -Defined relationships.
    -Added Public Column to Table.

---
+++ app/Models/Wishlist.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Wishlist extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+    ];
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    /**
+     * The sets belong to the collection.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function sets():BelongsToMany
+    {
+        return $this->belongsToMany(legoSet::class,'wishlist_lego_set','wishlist_id','lego_set_id');
+
+    }
+
+
+
+
+}
