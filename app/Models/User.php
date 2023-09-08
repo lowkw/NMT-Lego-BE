c7dff55fc88d888229ba7c0200389e91653ae661
NicholasL-Tafe <20101509@tafe.wa.edu.au>
2023-09-08 14:01:51 +0800

    SCRUM 30,31 Reviewed PR Changes 2 -
    Updated Wishlist User Model
    
    - Redefined Relationship of User to wishlist

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -48,8 +49,8 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Collection::class);
     }
-    public function wishlist(): BelongsTo
+    public function wishlist(): BelongsToMany
     {
-        return $this->belongsTo(Wishlist::class);
+        return $this->belongsToMany(Wishlist::class);
     }
 }
