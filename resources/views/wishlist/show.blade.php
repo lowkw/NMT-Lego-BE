a9fd1baeaa30b0b7c79feac9b67c3c8b78821efa
RowanNeoh <rowanneoh88au@gmail.com>
2023-10-03 11:25:50 +0800

    Scrum 42: -Add "remove" button in wishlist page

--- resources/views/wishlist/show.blade.php
+++ resources/views/wishlist/show.blade.php
@@ -109,8 +109,19 @@
                             <p class="card-text">
                             </p><h3 class="text-center">{{ $set->name }}</h3>
                             <p class="text-center">{{ $set->set_num }} | {{ $set->theme->name }}</p>
-                            <div class="d-grid gap-2 col-6 mx-auto">
+                            <div class="flex space-x-4 justify-center align-items-center mt-4">
+
                                 <a role="button" href="{{ route('sets.show', compact('set')) }}" class="btn btn-primary btn-sm">View Set -&gt;</a>
+                                <form action="{{ route('set.deleteWishlist',compact(['set'])) }}"
+                                      class="flex flex-col gap-4"
+                                      method="GET">
+                                    @csrf
+                                <button type="submit" role="button" class="btn btn-danger btn-sm">
+                                    Remove <i class="remove-icon fa-solid fa-square-minus"></i>
+                                </button>
+                                    <input type="hidden" name="oneWishlist" value="{{$wishlist->id}}" />
+                                </form>
+
                             </div>
                             <p></p>
                         </div>
