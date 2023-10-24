bf6dc6ccfa4d1be93a43583e4de3471073d2c3f1
Cameron <cameron@layup.co.za>
2023-10-24 12:32:41 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/views/js/app.js',
             ],
             refresh: true,
         }),
