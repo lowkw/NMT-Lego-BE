9668cbb7f7ec2e40612b4d24d819dc177d64a0e9
RowanNeoh <rowanneoh88au@gmail.com>
2023-11-03 20:46:57 +0800

    Add LinkedIn QR code and profile images

--- resources/views/aboutUs/index.blade.php
+++ resources/views/aboutUs/index.blade.php
@@ -9,34 +9,39 @@
 </section>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4" style="margin: 2rem;">
         <div class="bg-white p-4 rounded-lg shadow-md profileCard-aboutUs">
-            <img class="img-fluid rounded-circle mx-auto" src="http://localhost/images/default-profile-image.jpg" width="80%" alt="Profile Image">
+            <img class="img-fluid rounded-circle mx-auto" src="http://localhost/images/profile/cameron-photo.jpg" width="100%" alt="Profile Image">
             <div class="text-left mt-3">
                 <b>First Name:</b> Cameron<br>
                 <b>Last Name:</b> Morgan<br>
-                <b>LinkedIn:</b>
+                <b>Portfolio:</b> <a href="https://cmorgan.dev" target="_blank">Portfolio</a><br />
+                <b>LinkedIn:</b> <a href="https://www.linkedin.com/in/cameron-morgan-95846659" target="_blank">LinkedIn Profile</a><br />
+                <img class="img-fluid mx-auto" src="http://localhost/images/profile/QR-Cameron.png" width="70%" alt="Profile Image">
+
             </div>
         </div>
 
         <div class="bg-white p-4 rounded-lg shadow-md profileCard-aboutUs">
-            <img class="img-fluid rounded-circle mx-auto" src="http://localhost/images/default-profile-image.jpg" width="80%" alt="Profile Image">
+            <img class="img-fluid rounded-circle mx-auto" src="http://localhost/images/profile/rowan-neoh.jpg" width="95%" alt="Profile Image">
             <div class="text-left mt-3">
                 <b>First Name:</b> Shao Yung<br>
-                <b>Last Name:</b> Neoh<br>
-                <b>LinkedIn: </b> <a href="https://www.linkedin.com/in/rowanneoh/" target="_blank">LinkedIn Profile</a>
+                <b>Last Name:</b> Neoh (Rowan)<br>
+                <b>LinkedIn: </b> <a href="https://www.linkedin.com/in/rowanneoh/" target="_blank">LinkedIn Profile</a><br /><br />
+                <img class="img-fluid mx-auto" src="http://localhost/images/profile/QR-Rowan.png" width="70%" alt="Profile Image">
             </div>
         </div>
 
         <div class="bg-white p-4 rounded-lg shadow-md profileCard-aboutUs">
-            <img class="img-fluid rounded-circle mx-auto" src="http://localhost/images/default-profile-image.jpg" width="80%" alt="Profile Image">
+            <img class="img-fluid rounded-circle mx-auto" src="http://localhost/images/profile/kokweilow.jpg" width="100%" alt="Profile Image">
             <div class="text-left mt-3">
                 <b>First Name:</b> Kok Wei<br>
                 <b>Last Name:</b> Low<br>
-                <b>LinkedIn:</b> <a href="https://au.linkedin.com/in/kok-wei-low" target="_blank">LinkedIn Profile</a>
+                <b>LinkedIn:</b> <a href="https://au.linkedin.com/in/kok-wei-low" target="_blank">LinkedIn Profile</a><br /><br />
+                <img class="img-fluid mx-auto" src="http://localhost/images/profile/QR-kokWei.png" width="70%" alt="Profile Image">
             </div>
         </div>
 
         <div class="bg-white p-4 rounded-lg shadow-md profileCard-aboutUs">
-            <img class="img-fluid rounded-circle mx-auto" src="http://localhost/images/default-profile-image.jpg" width="80%" alt="Profile Image">
+            <img class="img-fluid rounded-circle mx-auto" src="http://localhost/images/default-profile-image.jpg" width="100%" alt="Profile Image">
             <div class="text-left mt-3">
                 <b>First Name:</b> Nicholas<br>
                 <b>Last Name:</b> Lumma<br>
